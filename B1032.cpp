431fd3eb34bf27999d3aa90d5226f71739e03b7f
zhuanshan <8434495+zhuanshan@user.noreply.gitee.com>
2022-01-07 09:55:18 +0800

    B1032

---
+++ B1032.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+const int maxn = 10010;
+int school[maxn]={0};
+int main(){
+    int n ,schid, score;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d%d",&schid,&score);
+        school[schid]+=score;
+    }
+    int k=1,max=-1;
+    for(int i=1;i<=n;i++){
+        if(school[i]>max){
+            max=school[i];
+            k=i;
+        }
+    }
+    printf("%d %d\n",k,max);
+    return 0;
+
+}
