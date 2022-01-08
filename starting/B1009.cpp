21f59d1467035e265d7dbf90af44b864abaf698d
zhuanshan <8434495+zhuanshan@user.noreply.gitee.com>
2022-01-08 10:02:08 +0800

    B1009

--- starting/B1009.cpp
+++ starting/B1009.cpp
@@ -6,5 +6,21 @@ int main(){
     gets(str);
     int len = strlen(str);
     int r=0,h=0;
-    
+    char ans[90][90];
+    for(int i=0;i<len;i++){
+        if(str[i]!=' '){
+            ans[r][h++]=str[i];   //str[r][h],r为一个一单词，h为一个一个字母
+        }else{
+            ans[r][h]='\0';
+            r++;
+            h=0;
+        }
+    }
+    for(int i=r;i>=0;i--){
+        printf("%s",ans[i]);
+        if(i>0){
+            printf(" ");
+        }
+    }
+    return 0;
 }
